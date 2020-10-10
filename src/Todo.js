47a8b22077216ca8d8e9ae3ea7c5b5eca624e055
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-10-10 10:33:53 +0530

    added input tag

--- src/Todo.js
+++ src/Todo.js
@@ -4,24 +4,38 @@ import './todo.css';
 const TodoItem = ({text, status}) => (
   <div className="todo-item">
     <div className={status === 'done' ? 'item-done' : 'item-undone'}></div>
-    <label className={status === 'done' ? 'item-text-done': null} >
+    <label className={status === 'done' ? 'item-text-done' : null}>
       {text}
     </label>
   </div>
 );
 
+const TodoItems = (props) => {
+  const items = props.items.map(({text, status}, index) => (
+    <TodoItem text={text} status={status} key={index} />
+  ));
+
+  return <div>{items}</div>;
+};
+
 class Todo extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {todoItems: [{text: 'Buy milk', status: 'done'}]};
+    this.state = {todoItems: [{text: 'Buy milk', status: 'done'}], text: ''};
+    this.handleInputValue = (e) => {this.handleInput(e.target.value)};
+  }
+
+  handleInput(text) {
+    this.setState(() => ({text}));
   }
 
   render() {
-    const items = this.state.todoItems.map(({text, status}) => (
-      <TodoItem text={text} status={status} />
-    ));
-
-    return <div>{items}</div>;
+    return (
+      <div>
+        <TodoItems items={this.state.todoItems} />
+        <input value={this.state.text} onChange={this.handleInputValue} />
+      </div>
+    );
   }
 }
 
